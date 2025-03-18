3af5f29b16645290427c58f4ac4723a4aec82728
Mohammad Rabin <jubayer_1228@yahoo.com>
2025-03-18 16:20:09 -0600

    updated params in run_bandit file

--- run_bandit.sh
+++ run_bandit.sh
@@ -1,8 +1,8 @@
 # Collect data
-python3 collect_data.py --env bandit --envs 1000 --H 100 --dim 5 --var 0.3 --cov 0.0 --envs_eval 200
+python3 collect_data.py --env bandit --envs 10000 --H 300 --dim 5 --var 0.3 --cov 0.0 --envs_eval 200
 
 # Train
-python3 train.py --env bandit --envs 1000 --H 100 --dim 5 --var 0.3 --cov 0.0 --lr 0.0001 --layer 2 --head 2 --shuffle --num_epochs 10 --seed 1
+python3 train.py --env bandit --envs 10000 --H 300 --dim 5 --var 0.3 --cov 0.0 --lr 0.0001 --layer 2 --head 2 --shuffle --num_epochs 100 --seed 1
 
 # Evaluate, choose an appropriate epoch
-python3 eval.py --env bandit --envs 1000 --H 100 --dim 5 --var 0.3 --cov 0.0 --lr 0.0001 --layer 2 --head 2 --shuffle --epoch 10 --n_eval 200 --seed 1
+python3 eval.py --env bandit --envs 10000 --H 300 --dim 5 --var 0.3 --cov 0.0 --lr 0.0001 --layer 2 --head 2 --shuffle --epoch 100 --n_eval 200 --seed 1
